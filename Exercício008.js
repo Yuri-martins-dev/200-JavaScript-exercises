8281de8fb61d4ac05c10e99737cc86b57e44013c
Yuri-martins-dev <yuriestudante2513@yahoo.com>
2025-09-23 12:28:56 -0300

    Declare uma variável e atribua um valor numérico a ela. Use o operador de incremento para aumentar o valor da variável. Imprima o resultado.

--- Exercício008.js
+++ Exercício008.js
@@ -1,4 +1,4 @@
 // Declare uma variável e atribua um valor numérico a ela. Use o operador de incremento para aumentar o valor da variável. Imprima o resultado.
 let a = 10;
-console.log (a++); // 10, pois mostra antes e incrementa depois.
-console.log (a);   // 11
+console.log (a++);  // 10, pois mostra antes e incrementa depois.
+console.log (a);    // 11
