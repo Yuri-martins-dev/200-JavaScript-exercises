62def4d43cc71aed463d50e7645a38021128cb21
Yuri-martins-dev <yuriestudante2513@yahoo.com>
2025-09-23 11:22:00 -0300

    Create Exercício005.js

---
+++ Exercício005.js
@@ -0,0 +1,5 @@
+//Declare duas variáveis e atribua valores numéricos a elas. Use o operador de adição para realizar operações entre as duas variáveis. Imprima o resultado.
+var número01 = 37;
+var número02 = 78;
+let soma = número01 + número02;
+console.log(soma); 
