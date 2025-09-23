cec4682288ab136fa56726a7e56b8282cfe45fb6
Yuri-martins-dev <yuriestudante2513@yahoo.com>
2025-09-23 11:44:34 -0300

    Create Exercicío006.js

---
+++ Exercicío006.js
@@ -0,0 +1,9 @@
+// Declare duas variáveis e atribua valores booleanos a elas. Use os operadores lógicos AND, OR e NOT para realizar operações lógicas entre as variáveis. Imprima os resultados.
+let valorA = true;
+let valorB = false;
+let conjunção = (valorA && valorB);
+let disjunção = (valorA || valorB);
+console.log ("Conjunção: " + conjunção);
+console.log ("Disjunção: " + disjunção);
+console.log ("Negação: " + !valorA);
+console.log ("Negação: " + !valorB);
