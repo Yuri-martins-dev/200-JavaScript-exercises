13bc9dfb39bd5f54d4fe61cbf03a90de932aba46
Yuri-martins-dev <yuriestudante2513@yahoo.com>
2025-10-17 15:51:29 -0300

    Declare duas variáveis chamadas "nome" e "sobrenome", atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira variável chamada "nomeCompleto" e imprima.

--- Exercício004.js
+++ Exercício004.js
@@ -1,1 +1,5 @@
 // Declare duas variáveis chamadas "nome" e "sobrenome", atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira variável chamada "nomeCompleto" e imprima.
+let nome = "Yuri";
+let sobreNome = "Martins";
+let nomeCompleto = (`O nome completo é: ${nome} ${sobreNome}.`);
+console.log (nomeCompleto);
