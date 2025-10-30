69b58f9492d2805985ff953cb15d9e89cdd9f204
Yuri-martins-dev <yuriestudante2513@yahoo.com>
2025-10-30 15:38:17 -0300

    Usando um laço while, crie um programa que adivinha um número que o usuário pensou, através do método de busca binária.

--- Exercício031.js
+++ Exercício031.js
@@ -1,22 +1,15 @@
 // Usando um laço while, crie um programa que adivinha um número que o usuário pensou, através do método de busca binária.
-let minimo = 1;
-let maximo = 100;
-let acertou = false;
+let min = 1;
+let max = 100;
+let chute = Math.floor((max = min) / 2);
+let numeroPensado = 50;
 
-alert("Pense em um número entre 1 e 100. Eu vou tentar adivinhar!");
-
-while (!acertou) {
-    let palpite = Math.floor((minimo + maximo) / 2);
-    let resposta = prompt(`O número é ${palpite}? (responda: "maior", "menor" ou "sim")`);
-
-    if (resposta === "sim") {
-        alert(`Acertei! O número é ${palpite}! 😎`);
-        acertou = true;
-    } else if (resposta === "maior") {
-        minimo = palpite + 1;
-    } else if (resposta === "menor") {
-        maximo = palpite - 1;
+while (chute !== numeroPensado) {
+    if(chute > numeroPensado) {
+        max = chute;
     } else {
-        alert('Responda apenas com "maior", "menor" ou "sim".');
+        min = chute;
     }
+    chute = Math.floor((max = min) / 2);
 }
+console.log("O número pensado é: " + chute);
