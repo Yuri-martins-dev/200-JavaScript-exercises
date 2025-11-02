55a692294d9d3e8993b2d69b4462aa4e03b3c548
Yuri-martins-dev <yuriestudante2513@yahoo.com>
2025-11-02 15:37:07 -0300

    Usando um laço do-while, crie um jogo de Pedra, papel e tesoura contra o computador.

--- Exercício032.js
+++ Exercício032.js
@@ -1,1 +1,7 @@
 // Usando um laço do-while, crie um jogo de "Pedra, papel e tesoura" contra o computador.
+alert("Vamos jogar pedra, papel, tesoura.")
+let escolha = prompt("Digite uma das seguintes opções: pedra, papel, tesoura.")
+escolha.toLowerCase()
+while (escolha !== "pedra" || "papel" ||  "tesoura") {
+    escolha = prompt("Digite apenas umas das opções a seguir: pedra, papel, ou tesoura.")
+}
